5d171b8dd5433fefb3847bcdd8f9d8a9c83d9313
Tygfeji <asapfeji@gmail.com>
2022-04-12 22:58:05 +0000

    02FNL

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,45 +1,20 @@
 #include "main.h"
-
-
-
-
-
 /**
-
- * main - Entry point
-
+ * main - main block
  *
-
- * Return: nothing, return void
-
+ * Description: print _putchar in a new line
+ * Return: 0
  */
-
-
-
 int main(void)
-
 {
-
-_putchar('_');
-
-_putchar('p');
-
-_putchar('u');
-
-_putchar('t');
-
-_putchar('c');
-
-_putchar('h');
-
-_putchar('a');
-
-_putchar('r');
-
-_putchar('\n');
-
-
-
-return (0);
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
